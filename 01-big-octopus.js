ab68d07518d89327d52aeb787a8e8579d22cc352
minkikim98 <minkikim98@gmail.com>
2021-01-27 14:42:57 -0800

    finished phases 1 and 2

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,22 +1,68 @@
+// const fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
+//   'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'];
+
+// console.log(quadraticBiggestFish(fishies));
+// console.log(nlognBiggestFish(fishies));
+// console.log(linearBiggestFish(fishies));
+
 function quadraticBiggestFish(fishes) {
-  // Code goes here ...
+
+  // Sluggish Octopus
+  // Find the longest fish in O(n^2) time. Do this by comparing all fish lengths to 
+  // all other fish lengths. Write your code inside the quadraticBiggestFish function.
+
+  for (let i = 0; i < fishes.length; i++) {
+    let foundLonger = false;
+    for (let j = 0; j < fishes.length; j++) {
+      if (fishes[j].length > fishes[i].length) {
+        foundLonger = true;
+        break;
+      }
+    }
+    if (!foundLonger) return fishes[i];
+  }
 }
 
 
 function nlognBiggestFish(fishes) {
   // Code goes here ...
+
+  //   Dominant Octopus
+  // Find the longest fish in O(n log n) time. Hint: Use the built in Array.sort()
+  //  method which runs in O(n log n). Remember that Big O is classified by the dominant term. 
+  //  Write your code inside the nlognBiggestFish function.
+  fishes.sort((a, b) => {
+    if (a.length > b.length) return -1;
+    else if (a.length < b.length) return 1;
+    else return 0;
+  });
+  return fishes[0];
 }
 
 
 function linearBiggestFish(fishes) {
+  // Clever Octopus
+  // Find the longest fish in O(n) time. The octopus can hold on to the longest fish that you have found so 
+  // far while stepping through the array only once. Write your code inside the linearBiggestFish function.
   // Code goes here ...
+
+  let longest = fishes[0];
+  for (let i = 1; i < fishes.length; i++) {
+    if (longest.length < fishes[i].length) {
+      longest = fishes[i];
+    }
+  }
+  return longest;
 }
 
 
 tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
 function slowDance(direction, tilesArray) {
-  // Code goes here ...
+  return tilesArray.indexOf(direction);
 }
+
+// console.log(slowDance("up", tilesArray));
+// console.log(slowDance("right-down", tilesArray));
 
 
 tilesObj = {
@@ -30,5 +76,8 @@ tilesObj = {
   "left-up": 7
 }
 function fastDance(direction, tilesObj) {
-  // Code goes here ...
+  return tilesObj[direction];
 }
+
+// console.log(fastDance("up", tilesObj));
+// console.log(fastDance("right-down", tilesObj));
--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,19 +1,67 @@
+console.log(fourthAnagram("gizmo", "sally"));
+console.log(fourthAnagram("elvis", "lives"));
+
 function firstAnagram(str1, str2) {
-  // Code goes here ....
+  for (let i = 0; i < str1.length; i++) {
+    if (str2.indexOf(str1[i]) === -1) return false;
+    str2 = str2.replace(str1[i], "");
+  }
+  if (str2.length === 0) return true;
+  else return false
 }
 
-
-
 function secondAnagram(str1, str2) {
-  // Code goes here ....
+  let str1Arr = str1.split("");
+  let str2Arr = str2.split("");
+  str1Arr.sort();
+  str2Arr.sort();
+
+  console.log(str1Arr);
+  console.log(str2Arr);
+
+  let str1Sorted = str1Arr.join("");
+  let str2Sorted = str2Arr.join("");
+
+  return str1Sorted === str2Sorted;
 }
 
 
 function thirdAnagram(str1, str2) {
-  // Code goes here ....
+  let obj1 = {}; 
+  let obj2 = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    if (obj1[str1[i]] === undefined) obj1[str1[i]] = 1;
+    else obj1[str1[i]]++;
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if (obj2[str2[i]] === undefined) obj2[str2[i]] = 1;
+    else obj2[str2[i]]++;
+  }
+
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 
 function fourthAnagram(str1, str2) {
-  // Code goes here ....
+  let obj = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (obj[str1[i]] === undefined) obj[str1[i]] = 1;
+    else obj[str1[i]]++;
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if (obj[str2[i]] === undefined) return false;
+    else obj[str2[i]]--;
+  }
+
+  return Object.values(obj).every(val => val === 0);
 }
