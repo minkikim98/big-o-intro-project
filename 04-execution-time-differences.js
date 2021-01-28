289ec4417885be7f4b5e045ab0fae151b84e1725
minkikim98 <minkikim98@gmail.com>
2021-01-27 16:37:34 -0800

    finished big O project

--- 04-execution-time-differences.js
+++ 04-execution-time-differences.js
@@ -1,23 +1,59 @@
 function myMin1a(list) {
-  // Code goes here ...
+  for (let i = 0; i < list.length; i++) {
+    let foundSmaller = false;
+    for (let j = 0; j < list.length; j++) {
+      if (list[j] < list[i]) foundSmaller = true;
+    }
+    if (!foundSmaller) return list[i];
+  }
 }
 
 
 function myMin1b(list) {
-  // Code goes here ...
 }
 
 
 function myMin2(list) {
-  // Code goes here ...
+  return list.reduce((accum, el) => {
+    if (el < accum) return el;
+    else return accum;
+  });
 }
 
 
 function largestContiguousSubsum1(array) {
-  // Code goes here ...
+  let subArrays = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length + 1; j++) {
+      subArrays.push(array.slice(i, j));
+    }
+  }
+  let max = subArrays.reduce((maxArr, subArr) => {
+    let maxArrSum = maxArr.reduce((accum, el) => accum + el);
+    let subArrSum = subArr.reduce((accum, el) => accum + el);
+    if (subArrSum > maxArrSum) return subArr;
+    else return maxArr;
+  });
+  return max.reduce((accum, el) => accum + el);
 }
 
 
 function largestContiguousSubsum2(array) {
-  // Code goes here ...
+  let max = 0;
+  let currSum = 0;
+  for (let i = 0; i < array.length; i++) {
+    currSum += array[i];
+    if (currSum > max) {
+      max = currSum;
+    }
+    if (currSum < 0) {
+      currSum = 0;
+    }
+  }
+  return max;
 }
+
+const list = [0, 3, 5, 4, -5, -7, 1, 90];
+
+const array = [5, 3, -7, -3, 7, -5];
+console.log(largestContiguousSubsum2(list));
